3d86fd35993146bbd445ef70deca216984376eb3
princeraja77 <rajaprince941@gmail.com>
2024-08-23 22:35:41 +0530

    some changes

---
+++ Touch.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Touch{
+    public static void main(String args[]){
+        int a=10;
+        int b=10;
+        int c=a+b;
+        System.out.println(c);
+        // aksejfbkajfhbv
+    }
+}
